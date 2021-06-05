1528e50f02202b0b921bb100128b733a4d3c2aa5
Athulpreman <Athulpreman9562473440@gmail.com>
2021-06-05 13:03:58 -0700

    day4Assign

--- eda.py
+++ eda.py
@@ -1,28 +1,49 @@
-#import libraries
-import matplotlib.pyplot as plt 
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib
-#matplotlib.use('Agg')
-import seaborn as sns 
+import seaborn as sns
+
 #Remove Warnings
 st.balloons()
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title("Tips_Dataset")
-#import dataset
-df = pd.read_csv('tips.csv')
-#First thirty rows
-tips = df.head(10)
-#Display the table
-st.table(tips)
-#############
+st.title("Dataset")
+
+df = pd.read_csv("titanic.csv")
+
+titanic = df.head(20)
+
+st.table(titanic)
+st.header("Visualisation Using Seaborn")
+
 #bar plot
 st.subheader("Bar Plot")
-tips.plot(kind='bar')
+titanic.plot(kind='bar')
 st.pyplot()
-################
+
+#Displot
+st.subheader("Displot")
+sns.displot(titanic['Survived'])
+st.pyplot()
+
+#joinplot
+st.subheader("JointPlot")
+sns.jointplot(x='Age',y='Survived',data=titanic,kind='scatter')
+st.pyplot()
+
 #pairplot
 st.subheader("Pairplot")
-sns.pairplot(tips,hue='sex',palette='rainbow')
+sns.pairplot(titanic,hue='Sex',palette='rainbow')
+st.pyplot()
+
+#Rugplot
+st.subheader("Rugplot")
+sns.rugplot(titanic['Survived'])
+st.pyplot()
+
+#Correation
+st.subheader("Heatmap")
+sns.heatmap(titanic.corr(),cmap='coolwarm',annot=True)
 st.pyplot()
